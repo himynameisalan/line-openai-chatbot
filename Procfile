1e22daa4c781a8b4dc3d4f10e838b279d825e3bb
1300360a <alanhuang@softmobile.com.tw>
2023-05-03 12:13:18 +0800

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn line-openai-chatbot:app
---
+++ line-openai-chatbot.py
@@ -0,0 +1,50 @@
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+import openai
+import os
+
+# LINE Messaging API
+line_bot_api = LineBotApi(os.environ.get("LINE_CHANNEL_ACCESS_TOKEN"))
+handler = WebhookHandler(os.environ.get("LINE_CHANNEL_SECRET"))
+
+# OpenAI API
+# openai.api_key = os.environ.get("OPENAI_API_KEY")
+
+app = Flask(__name__)
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    signature = request.headers["X-Line-Signature"]
+    body = request.get_data(as_text=True)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return "OK"
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_text_message(event):
+    user_message = event.message.text
+    # openai_response = get_openai_response(user_message)
+    line_bot_api.reply_message(
+        # event.reply_token, TextSendMessage(text=openai_response)
+        event.reply_token, TextSendMessage(text=user_message)
+    )
+
+def get_openai_response(prompt):
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=150,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    return response.choices[0].text.strip()
+
+if __name__ == "__main__":
+    app.run()
